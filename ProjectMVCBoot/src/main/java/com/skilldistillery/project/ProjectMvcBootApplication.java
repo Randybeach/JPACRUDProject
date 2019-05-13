61fe61a51b0baa0d465e9fae679d8a48e9e3cc4c
Randybeach <beach.randyj@gmail.com>
2019-05-13 10:27:39 -0600

    added war task

--- ProjectMVCBoot/src/main/java/com/skilldistillery/project/ProjectMvcBootApplication.java
+++ ProjectMVCBoot/src/main/java/com/skilldistillery/project/ProjectMvcBootApplication.java
@@ -2,10 +2,17 @@ package com.skilldistillery.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ProjectMvcBootApplication {
-
+public class ProjectMvcBootApplication extends SpringBootServletInitializer {
+	
+	  @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(ProjectMvcBootApplication.class);
+	   
+	  }
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectMvcBootApplication.class, args);
 	}
